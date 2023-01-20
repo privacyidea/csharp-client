19fb5594a29535b483e8a5ba94ee3d2890109a3b
lukasmatusiewicz <77617779+lukasmatusiewicz@users.noreply.github.com>
2023-01-20 12:18:37 +0100

    Update PIChallenge.cs

--- PrivacyIDEA_Client/PIChallenge.cs
+++ PrivacyIDEA_Client/PIChallenge.cs
@@ -6,6 +6,7 @@ namespace PrivacyIDEA_Client
     {
         public string Serial { get; set; } = "";
         public string Message { get; set; } = "";
+        public string ClientMode { get; set; } = "";
         public string TransactionID { get; set; } = "";
         public string Type { get; set; } = "";
         public Dictionary<string, object> Attributes { get; set; } = new Dictionary<string, object>();
